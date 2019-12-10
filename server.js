a1a5482888182cd6b2de09e5752104d1de5f2f20
Joanna Sowa <joanna.sowa19@gmail.com>
2019-12-10 12:54:41 -0600

    edits

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+// Dependencies
 const express = require("express");
 const app = express();
+const htmlRoutes = require("./routes/htmlRoutes");
 const PORT = process.env.PORT || 3000;
 
+// Parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use("/", htmlRoutes);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
